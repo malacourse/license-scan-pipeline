41e5f89034e745f299f3267115f4508448f688e5
Mike LaCourse <mlacours@redhat.com>
2018-05-29 14:26:34 -0400

    add pipeline file

---
+++ pipeline.groovy
@@ -0,0 +1,34 @@
+pipeline {
+  // Use Jenkins Maven slave
+  // Jenkins will dynamically provision this as OpenShift Pod
+  // All the stages and steps of this Pipeline will be executed on this Pod
+  // After Pipeline completes the Pod is killed so every run will have clean
+  // workspace
+  //agent {
+  //  label 'maven'
+  //}
+
+  // Pipeline Stages start here
+  // Requeres at least one stage
+  stages {
+
+    // Checkout source code
+    // This is required as Pipeline code is originally checkedout to
+    // Jenkins Master but this will also pull this same code to this slave
+    stage('Git Checkout') {
+      steps {
+        // Turn off Git's SSL cert check, uncomment if needed
+        // sh 'git config --global http.sslVerify false'
+        git url: "${APPLICATION_SOURCE_REPO}"
+      }
+    }
+
+    // Run Maven build, skipping tests
+    stage('Build'){
+      steps {
+        sh "ls -lrt"
+      }
+    }
+
+  }
+}
