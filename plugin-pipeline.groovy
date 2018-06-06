83ff9ff261859bb6f0c85280856988b771a0025e
Michael LaCourse <mlacours@redhat.com>
2018-06-06 17:01:53 -0400

    add rc

--- plugin-pipeline.groovy
+++ plugin-pipeline.groovy
@@ -1,9 +1,5 @@
 pipeline {
   // Use Jenkins Maven slave
-  // Jenkins will dynamically provision this as OpenShift Pod
-  // All the stages and steps of this Pipeline will be executed on this Pod
-  // After Pipeline completes the Pod is killed so every run will have clean
-  // workspace
   agent {
     label 'maven'
   }
@@ -11,10 +7,24 @@ pipeline {
   // Pipeline Stages start here
   // Requeres at least one stage
   stages {
-    //def nexusurl = "http://nexus-cicd.192.168.99.100.nip.io/repository/lm-approved/"
 
-    // Checkout source code
-    // This is required as Pipeline code is originally checkedout to
+    stage ('init')
+    {
+      steps {
+        script {
+          env.NEXUS_URL = 'http://nexus-cicd.apps.mikelacourse.com'
+          env.RC_URL = 'http://rocketchat-rocket-chat.apps.mikelacourse.com'
+          env.RC_USER = 'D69DWkjdqW6QdmNmy'
+          env.RC_TOKEN = 'mphgaHUUq701k8_zsZSe7vNSYa9iaUxlX3yORXJtqH6'
+          //env.HUB_URL = 'https://bizdevhub.blackducksoftware.com'
+          //env.HUB_TOKEN = 'NDM2ODEwN2MtMWZkMC00MTAwLTgyNDItMzViMGY1ZDQ2YzdkOjM4OTVlMTA0LTk3ZjMtNDEzYS05ZjdiLWExYjhkNjgwYWY0Mg=='
+          env.HUB_URL = 'https://redhathub.blackducksoftware.com'
+          env.HUB_TOKEN = 'NDM2ODEwN2MtMWZkMC00MTAwLTgyNDItMzViMGY1ZDQ2YzdkOjM4OTVlMTA0LTk3ZjMtNDEzYS05ZjdiLWExYjhkNjgwYWY0Mg=='
+        }
+      }
+    }
+
+
     // Jenkins Master but this will also pull this same code to this slave
     stage('Git Checkout') {
       steps {
@@ -29,17 +39,14 @@ pipeline {
   stage('Scan') {
      steps {
 
-        //sh "mvn package"
-        //hub_detect hubParams + '--detect.project.name="Redhat Mike Java 1" '
-    
         print "USING DIR: ${CONTEXT_DIR}"
         sh "ls -lrt ${CONTEXT_DIR}"  
     
         sh "mkdir ./scanreports"
         dir("${CONTEXT_DIR}")
         {
-          hub_detect '--blackduck.hub.url="https://bizdevhub.blackducksoftware.com" \
-            --blackduck.hub.api.token="NjZhMmM3ODYtODQ1Ni00ZTRiLWE4ZGUtZjljOTYwMzVmZjdkOjNhNDk2ZmU1LTQyOWQtNDMxYi1hYzljLThiZWMwYzliNTc1ZA==" \
+          hub_detect '--blackduck.hub.url="${HUB_URL}" \
+            --blackduck.hub.api.token="${HUB_TOKEN}" \
             --detect.project.name="${ARTIFACT_NAME}" \
             --detect.policy.check.fail.on.severities=BLOCKER,CRITICAL --detect.risk.report.pdf=true \
             --detect.risk.report.pdf.path="./scanreports/" \
@@ -58,7 +65,25 @@ pipeline {
    stage('Verify Report') 
    {
       steps {
+        script {
+            def message = "Please review ${ARTIFACT_NAME} located at ${HUB_URL} and proceed to Openshift to approve/reject the requrest"
+           sh """
+            curl -H "X-Auth-Token: ${RC_TOKEN}" -H "X-User-Id: ${RC_USER}" -H "Content-type:application/json" ${RC_URL}/api/v1/chat.postMessage -d '{ "channel": "#needs-approval", "text": "${message}" }'
+              """
+        }
         input( message: "Approve ${ARTIFACT_NAME}?")
+      }
+   }
+
+   stage('Sign Artifact')
+   {
+      steps {
+        script {
+          dir ("${CONTEXT_DIR}")
+          {
+            print "Add signature to ${ARTIFACT_NAME}.zip"
+          }
+        }
       }
    }
 
@@ -68,7 +93,7 @@ pipeline {
         script {
           dir ("${CONTEXT_DIR}")
           {
-            def nexusurl = "http://nexus-cicd.apps.mikelacourse.com/repository/lm-approved/"
+            def nexusurl = "${NEXUS_URL}/repository/lm-approved/"
             def todaysdate = new Date()
             uploadPath = todaysdate.format("YYYY/MM/dd/HH-mm-ss");
             print uploadPath
@@ -77,7 +102,23 @@ pipeline {
             sh "curl -k -u admin:admin123 -X PUT " + nexusurl + uploadPath + "/scan-report.pdf" + " -T " + reportPath
         
             sh "zip ${ARTIFACT_NAME}.zip -r ."
-            sh "curl -k -u admin:admin123 -X PUT " + nexusurl + uploadPath + "/${ARTIFACT_NAME}.zip" + " -T ${ARTIFACT_NAME}.zip" 
+            sh "curl -k -u admin:admin123 -X PUT " + nexusurl + uploadPath + "/${ARTIFACT_NAME}.zip" + " -T ${ARTIFACT_NAME}.zip"
+            env.NEXUS_ARTIFACT_URL = nexusurl + uploadPath 
+          }
+        }
+      }
+   }
+
+   stage('Notify Users')
+   {
+      steps {
+        script {
+          dir ("${CONTEXT_DIR}")
+          {
+            def message = "The following artifacts have been approved: ${ARTIFACT_NAME}. They can be accessed at ${NEXUS_ARTIFACT_URL}"
+           sh """
+            curl -H "X-Auth-Token: ${RC_TOKEN}" -H "X-User-Id: ${RC_USER}" -H "Content-type:application/json" ${RC_URL}/api/v1/chat.postMessage -d '{ "channel": "#needs-approval", "text": "${message}" }'
+              """
           }
         }
       }
